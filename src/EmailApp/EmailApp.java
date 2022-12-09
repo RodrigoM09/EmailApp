cc054c2b5a47039b0e4a65b1da0d455335b2739e
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-09 12:56:18 -0600

    random password generator

---
+++ src/EmailApp/EmailApp.java
@@ -0,0 +1,9 @@
+package EmailApp;
+
+public class EmailApp {
+    public static class Main {
+        public static void main(String[] args) {
+            Email email = new Email("John","Smith");
+        }
+    }
+}
